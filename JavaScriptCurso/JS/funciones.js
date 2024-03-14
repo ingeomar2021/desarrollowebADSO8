8bd667bed56476bf662a1630e158a66a4d805e46
Omar Gutierrez <ingeomar2001@gmail.com>
2024-03-14 11:26:03 -0500

    Clase del 43 de marzo

---
+++ JavaScriptCurso/JS/funciones.js
@@ -0,0 +1,14 @@
+document.querySelector("#boton1").addEventListener("click",()=>{
+    let ob1=document.querySelector(".mensaje");
+    ob1.style.backgroundColor="#ff0";
+    let ob2=document.querySelector("#lista1");
+    ob2.style.backgroundColor="#0ff";
+    
+    let lista=document.querySelectorAll("ol li");
+    for (const elemento of lista) {
+        elemento.style.backgroundColor="#ff0";
+        
+    }
+
+    
+})
