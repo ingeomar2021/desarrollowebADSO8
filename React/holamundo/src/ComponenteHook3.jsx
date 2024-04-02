a288fe7d19c69228585f56501747a94d91a25f25
Omar Gutierrez <ingeomar2001@gmail.com>
2024-04-02 08:20:59 -0500

    Clase del 2 de Abril

--- React/holamundo/src/ComponenteHook3.jsx
+++ React/holamundo/src/ComponenteHook3.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
 
 function ComponenteHook3() {
+
+    function eliminarUltimaFila() {
+        if (articulos.length>0) {
+            const temp=Array.from(articulos)
+            temp.pop()
+            setArticulos(temp)
+        }
+    }
+
+    function borrar(cod) {
+        const temp=articulos.filter((articulo)=>articulo.codigo!=cod)
+        setArticulos(temp)        
+    }
+
+
     const[articulos,setArticulos]=useState([{
         codigo:1,
         descripcion:"Papas",
@@ -24,19 +39,22 @@ function ComponenteHook3() {
                 <th>Código</th>
                 <th>Descripcion</th>
                 <th>Precio</th>
+                <th>Borrar</th>
             </thead>
             <tbody>
                 {articulos.map(articulo=>{
                     return(
-                        <tr>
+                        <tr key={articulo.codigo}>
                             <td>{articulo.codigo}</td>
                             <td>{articulo.descripcion}</td>
                             <td>{articulo.precio}</td>
+                            <td><button onClick={()=>borrar(articulo.codigo)}>Borrar</button></td>
                         </tr>
                     )
                 })}
             </tbody>
         </table>
+        <button onClick={eliminarUltimaFila}>Eliminar Ultima Fila</button>
         
         </>
     )
